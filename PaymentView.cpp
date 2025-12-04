8a61802b4740aae7c30d72211035eaf3d569b303
SMarco2310 <sossoumarcetienne@icloud.com>
2025-12-04 14:25:36 +0000

    Payment page

---
+++ PaymentView.cpp
@@ -0,0 +1,2 @@
+#include "PaymentView.h"
+
---
+++ PaymentView.h
@@ -0,0 +1,238 @@
+﻿#pragma once
+
+namespace Group7FinalProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for PaymentView
+	/// </summary>
+	public ref class PaymentView : public System::Windows::Forms::Form
+	{
+	public:
+		PaymentView(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~PaymentView()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem2;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem3;
+	private: System::Windows::Forms::ToolStripMenuItem^ profileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ systemSetUpToolStripMenuItem;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Panel^ panel3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem2 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem3 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->profileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->systemSetUpToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->menuStrip1->SuspendLayout();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->panel3->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->menuStrip1->GripMargin = System::Windows::Forms::Padding(2, 2, 0, 2);
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(28, 28);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(5) {
+				this->toolStripMenuItem1,
+					this->toolStripMenuItem2, this->toolStripMenuItem3, this->profileToolStripMenuItem, this->systemSetUpToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1458, 38);
+			this->menuStrip1->TabIndex = 2;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(168, 38);
+			this->toolStripMenuItem1->Text = L"📕My Courses";
+			// 
+			// toolStripMenuItem2
+			// 
+			this->toolStripMenuItem2->Name = L"toolStripMenuItem2";
+			this->toolStripMenuItem2->Size = System::Drawing::Size(149, 38);
+			this->toolStripMenuItem2->Text = L"🧾Transcript";
+			// 
+			// toolStripMenuItem3
+			// 
+			this->toolStripMenuItem3->Name = L"toolStripMenuItem3";
+			this->toolStripMenuItem3->Size = System::Drawing::Size(178, 38);
+			this->toolStripMenuItem3->Text = L"💳Fee Payment";
+			// 
+			// profileToolStripMenuItem
+			// 
+			this->profileToolStripMenuItem->Name = L"profileToolStripMenuItem";
+			this->profileToolStripMenuItem->Size = System::Drawing::Size(119, 38);
+			this->profileToolStripMenuItem->Text = L"👤Profile";
+			// 
+			// systemSetUpToolStripMenuItem
+			// 
+			this->systemSetUpToolStripMenuItem->Name = L"systemSetUpToolStripMenuItem";
+			this->systemSetUpToolStripMenuItem->Size = System::Drawing::Size(134, 38);
+			this->systemSetUpToolStripMenuItem->Text = L"⚙️Settings";
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Location = System::Drawing::Point(47, 164);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(777, 242);
+			this->panel1->TabIndex = 3;
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->label3);
+			this->panel2->Location = System::Drawing::Point(911, 164);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(455, 670);
+			this->panel2->TabIndex = 4;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(48, 570);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 72;
+			this->dataGridView1->RowTemplate->Height = 31;
+			this->dataGridView1->Size = System::Drawing::Size(776, 614);
+			this->dataGridView1->TabIndex = 5;
+			// 
+			// panel3
+			// 
+			this->panel3->Controls->Add(this->label4);
+			this->panel3->Location = System::Drawing::Point(49, 452);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(775, 112);
+			this->panel3->TabIndex = 6;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(52, 79);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(279, 48);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Fee Payment";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(25, 28);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(279, 48);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"Fee Payment";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(36, 42);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(279, 48);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"Fee Payment";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(14, 20);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(279, 48);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Fee Payment";
+			// 
+			// PaymentView
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1458, 1299);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->panel3);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->menuStrip1);
+			this->Name = L"PaymentView";
+			this->Text = L"PaymentView";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->panel3->ResumeLayout(false);
+			this->panel3->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
