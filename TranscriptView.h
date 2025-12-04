d3c7b04cbfd0479227ef147940226249cc9913c3
SMarco2310 <sossoumarcetienne@icloud.com>
2025-12-04 14:18:18 +0000

    transcript view

--- TranscriptView.h
+++ TranscriptView.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace Group7FinalProject {
 
@@ -34,6 +34,16 @@ namespace Group7FinalProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem2;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem3;
+	private: System::Windows::Forms::ToolStripMenuItem^ profileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ systemSetUpToolStripMenuItem;
+	private: System::Windows::Forms::PrintPreviewControl^ printPreviewControl1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,11 +58,106 @@ namespace Group7FinalProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"TranscriptView";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem2 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem3 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->profileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->systemSetUpToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->printPreviewControl1 = (gcnew System::Windows::Forms::PrintPreviewControl());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->menuStrip1->GripMargin = System::Windows::Forms::Padding(2, 2, 0, 2);
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(28, 28);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(5) {
+				this->toolStripMenuItem1,
+					this->toolStripMenuItem2, this->toolStripMenuItem3, this->profileToolStripMenuItem, this->systemSetUpToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1107, 38);
+			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(168, 34);
+			this->toolStripMenuItem1->Text = L"📕My Courses";
+			// 
+			// toolStripMenuItem2
+			// 
+			this->toolStripMenuItem2->Name = L"toolStripMenuItem2";
+			this->toolStripMenuItem2->Size = System::Drawing::Size(149, 34);
+			this->toolStripMenuItem2->Text = L"🧾Transcript";
+			// 
+			// toolStripMenuItem3
+			// 
+			this->toolStripMenuItem3->Name = L"toolStripMenuItem3";
+			this->toolStripMenuItem3->Size = System::Drawing::Size(178, 34);
+			this->toolStripMenuItem3->Text = L"💳Fee Payment";
+			// 
+			// profileToolStripMenuItem
+			// 
+			this->profileToolStripMenuItem->Name = L"profileToolStripMenuItem";
+			this->profileToolStripMenuItem->Size = System::Drawing::Size(119, 34);
+			this->profileToolStripMenuItem->Text = L"👤Profile";
+			// 
+			// systemSetUpToolStripMenuItem
+			// 
+			this->systemSetUpToolStripMenuItem->Name = L"systemSetUpToolStripMenuItem";
+			this->systemSetUpToolStripMenuItem->Size = System::Drawing::Size(134, 34);
+			this->systemSetUpToolStripMenuItem->Text = L"⚙️Settings";
+			// 
+			// printPreviewControl1
+			// 
+			this->printPreviewControl1->Location = System::Drawing::Point(65, 295);
+			this->printPreviewControl1->Name = L"printPreviewControl1";
+			this->printPreviewControl1->Size = System::Drawing::Size(1005, 1027);
+			this->printPreviewControl1->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(60, 142);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(421, 48);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Academic Transcript";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(833, 153);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(216, 56);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"🖨️  Print Transcript";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// TranscriptView
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(11, 24);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1107, 1424);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->printPreviewControl1);
+			this->Controls->Add(this->menuStrip1);
+			this->Name = L"TranscriptView";
+			this->Text = L"TranscriptView";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
