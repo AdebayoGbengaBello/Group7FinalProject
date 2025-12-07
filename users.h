b2edfd4e98500c69a01f800c3f67e1d1078b4ca8
AdebayoGbengaBello <adebayo.bello@ashesi.edu.gh>
2025-12-07 21:56:09 +0000

    Remove obsolete classes

--- users.h
+++ users.h
@@ -7,20 +7,3 @@ public ref class User {
 		String^ email;
 		String^ password;
 };
-public ref class AdminStaff : public User {
-	public:
-		int staffID;
-		int deptID;
-};
-public ref class Faculty : public User {
-	public:
-		int facultyID;
-		int deptID;
-		String^ position;
-};
-public ref class Student : public User {
-	public:
-		int studentID;
-		int currentLevel;
-		String^ semester;
-};
